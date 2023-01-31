b1c11582df59b6768ae5cc56b1d06572ab74e91e
joao <jp_santana00@outlook.com>
2023-01-31 13:43:20 -0300

    add nodemom and post

--- src/server.js
+++ src/server.js
@@ -1,17 +1,12 @@
 const express = require( 'express' );
 
 const app = express();
+app.use(express.json());
 
-app.get("/message/:id/:user", (request, response) => {
-  const{ id, user} = request.params;
+app.post("/users", (request, response) => {
+  const { name, email, password } = request.body;
 
-  response.send(`Mensagem ID: ${id}. Para o usuario: ${user}.`);
-});
-
-app.get("/users", (request, response) => {
-  const{ page, limit} = request.query;
-
-  response.send(`Pagina: ${page}. Mostrar: ${limit}`);
+  response.json({ name, email, password });
 });
 
 const PORT = 3333;
